df33645eea07cb76e5cdc9056e377dfbc243c53d
Jonathan Leavitt <jon.s.leavitt@gmail.com>
2012-01-14 23:55:37 -0500

    minor

---
+++ personal/jon.el
@@ -0,0 +1,39 @@
+;;indents the whole buffer
+(defun ib ()
+  "indent whole buffer"
+  (interactive)
+  (delete-trailing-whitespace)
+  (indent-region (point-min) (point-max) nil)
+  (untabify (point-min) (point-max)))
+
+;;put all buffers in one window
+(setq ns-pop-up-frames nil)
+
+;;allows sublime text-like autocompletions
+(require 'cl)
+(defvar my-abbrev-tables nil)
+(defun my-abbrev-hook ()
+  (let ((def (assoc (symbol-name last-abbrev) my-abbrev-tables)))
+    (when def
+      (execute-kbd-macro (cdr def)))
+    t))
+(put 'my-abbrev-hook 'no-self-insert t)
+(defmacro declare-abbrevs (table abbrevs)
+  (if (consp table)
+      `(progn ,@(loop for tab in table
+                      collect `(declare-abbrevs ,tab ,abbrevs)))
+    `(progn
+       ,@(loop for abbr in abbrevs
+               do (when (third abbr)
+                    (push (cons (first abbr) (read-kbd-macro (third abbr)))
+                          my-abbrev-tables))
+               collect `(define-abbrev ,table
+                          ,(first abbr) ,(second abbr) ,(and (third abbr)
+                                                             ''my-abbrev-hook))))))
+(put 'declare-abbrevs 'lisp-indent-function 2)
+
+(autoload 'expand-abbrev-hook "expand")
+(define-abbrev-table 'java-mode-abbrev-table'(
+                                             ("sout" ["System.out.println(\"\");" 3 () nil] expand-abbrev-hook 0)
+                                             ("psvm" ["public static void main(String args[]){\n\n}" 2 () nil] expand-abbrev-hook 0)))
+
