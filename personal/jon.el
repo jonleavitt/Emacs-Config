93c385e4bb68b1342bab76e31ddbe1ef61c8312f
Jonathan Leavitt <jon.s.leavitt@gmail.com>
2012-02-15 12:25:39 -0500

    added C-x C-k for server edit kills

--- personal/jon.el
+++ personal/jon.el
@@ -48,6 +48,6 @@
      (list (line-beginning-position)
            (line-beginning-position 2)))))
 
-(remove-hook 'kill-buffer-query-functions 'server-kill-buffer-query-function)
+;;(remove-hook 'kill-buffer-query-functions 'server-kill-buffer-query-function)
 ;;prevent annoying still has clients message
-                                        ;(global-set-key (kbd "C-x C-k") 'server-edit)
+(global-set-key (kbd "C-x C-k") 'server-edit)
