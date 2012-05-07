f4fb49368dc07c28e18eaf34671c3c2925088821
Jonathan Leavitt <jon.s.leavitt@gmail.com>
2012-05-06 22:52:43 -0400

    ?

--- modules/prelude-latex.el
+++ modules/prelude-latex.el
@@ -1,40 +1,10 @@
-;;; prelude-latex.el --- Emacs Prelude: Sane setup for LaTeX writers.
-;;
-;; Copyright (c) 2011 Bozhidar Batsov
-;;
-;; Author: Bozhidar Batsov <bozhidar.batsov@gmail.com>
-;; URL: http://www.emacswiki.org/cgi-bin/wiki/Prelude
-;; Version: 1.0.0
-;; Keywords: convenience
-
-;; This file is not part of GNU Emacs.
-
-;;; Commentary:
-
-;; Nice defaults for the premium LaTeX editing mode auctex.
-
-;;; License:
-
-;; This program is free software; you can redistribute it and/or
-;; modify it under the terms of the GNU General Public License
-;; as published by the Free Software Foundation; either version 3
-;; of the License, or (at your option) any later version.
-;;
-;; This program is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;; GNU General Public License for more details.
-;;
-;; You should have received a copy of the GNU General Public License
-;; along with GNU Emacs; see the file COPYING.  If not, write to the
-;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
-;; Boston, MA 02110-1301, USA.
-
-;;; Code:
-
 ;; AUCTeX configuration
 (setq TeX-auto-save t)
 (setq TeX-parse-self t)
+
+;;added by jon
+(load "preview-latex.el" nil t t)
+
 
 (setq-default TeX-master nil)
 
